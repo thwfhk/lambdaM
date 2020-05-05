7635b0199f90f062046099a76e45e3ccc6049728
thwfhk <thwfhk@163.com>
2020-05-05 17:49:31 +0800

    clean up

--- init.ml
+++ init.ml
@@ -3,6 +3,4 @@
 #load "metric.cmo"
 #load "eval.cmo"
 #load "typecheck.cmo"
-#use "examples_metric.ml"
-(* #use "examples_vector.ml" *)
-(* #use "examples_nat.ml" *)
+#use "examples.ml"
--- typecheck.mli
+++ typecheck.mli
@@ -8,6 +8,6 @@ open Print
 val typeof : context -> metricContext -> term -> ty 
 val kindof : context -> ty -> kind 
 val checkkind : context -> kind -> unit 
-val kindeqv : context -> kind -> kind -> bool 
+(* val kindeqv : context -> kind -> kind -> bool  *)
 val tyeqv : context -> ty -> ty -> bool 
 val tmeqv : context -> term -> term -> bool
